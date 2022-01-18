714a1cf415a0f0b52fa1d37682eceb84ae948bcb
diego <diegovillate950@gmail.com>
2022-01-17 20:27:04 -0500

    Estructura App

---
+++ lib/screens/recents_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RecentsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:seriesapp/screens/home_screen.dart';
+export 'package:seriesapp/screens/details_screen.dart';
